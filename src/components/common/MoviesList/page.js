34f920fd6d9d3309c36f035047c155070c516667
octavius2929 <justinoctavio2001@gmail.com>
2021-05-17 19:56:11 -0400

    fixing height in MoviesList

--- src/components/common/MoviesList/page.js
+++ src/components/common/MoviesList/page.js
@@ -4,7 +4,14 @@ import Title from '../Title';
 
 const MoviesListPage = ({ movies, title, main, onClick }) => {
   return (
-    <Block bg="third" width="100vw" style={{ margin: '1em 0' }} flex column>
+    <Block
+      bg="third"
+      width="100vw"
+      height="max-content"
+      style={{ margin: '1em 0' }}
+      flex
+      column
+    >
       <Block p1 className="list_title">
         <Title logo dataTest="Title">
           {title}
